da9fa02d4a192d5d9b7e842940bb23b78320b2ae
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-10-05 15:36:17 +0530

    Update tuple.py

--- Ds_labcycle1/tuple.py
+++ Ds_labcycle1/tuple.py
@@ -7,3 +7,21 @@ t=(5,6)
 print("Tuple t:",t)
 print(d[t])
 print(d[1,2])
+
+//OUTPUT_______________________
+
+Conainers:Tuples
+Dictionary with tuple keys:
+(0, 1) : 0
+(1, 2) : 1
+(2, 3) : 2
+(3, 4) : 3
+(4, 5) : 4
+(5, 6) : 5
+(6, 7) : 6
+(7, 8) : 7
+(8, 9) : 8
+(9, 10) : 9
+Tuple t: (5, 6)
+5
+1
