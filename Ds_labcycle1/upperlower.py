6743509084cfb8d653d7afffc012af898790b260
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-10-05 15:42:18 +0530

    Add files via upload

---
+++ Ds_labcycle1/upperlower.py
@@ -0,0 +1,11 @@
+class greeter:
+    def __init__(self,name):
+        self.name = name
+    def greet(self,loud=False):
+        if loud:
+            print("HELLO",self.name.upper())
+        else:
+            print("Hello",self.name.lower())
+g=  greeter('fred')
+g.greet()
+g.greet(loud=True)
