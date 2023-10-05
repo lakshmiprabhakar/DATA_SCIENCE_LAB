62112afd09514a4661120c9e400e78e92b1322d4
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-10-05 13:54:08 +0530

    Update arithmetic.py

--- arithmetic.py
+++ arithmetic.py
@@ -9,3 +9,14 @@ print("Addition :",x+y)
 print("Subtraction:",x-y)
 print("Multiplication:",x*y)
 print("Exponentiation:",x**2)
+
+//OUTPUT
+Demo of basic data types : Numbers
+x= 3
+y= 2.5
+Data type of variable x : <class 'int'>
+Data type of variable y : <class 'float'>
+Addition : 5.5
+Subtraction: 0.5
+Multiplication: 7.5
+Exponentiation: 9
