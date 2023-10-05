c608a809d01955bbb862296cdc111ea0d76cd7ad
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-10-05 15:01:21 +0530

    Add files via upload

---
+++ Ds_labcycle1/string.py
@@ -0,0 +1,13 @@
+print("Demo of basic data types : String")
+s="Hello"
+t="World"
+print("String 1 :",s)
+print("String 2 :",t)
+d=s +"" +t
+print("String Concatenation :",d)
+print("Capitalize :",d.capitalize())
+print("Converted to upper case :",s.upper())
+print("Right justify a String :",s.rjust(7))
+print("String at center :",s.center(7))
+print("After replacing l with ell :",s.replace("l",'(ell)'))
+print("String after striping leading and trailing white spaces: ",'world'.strip())
