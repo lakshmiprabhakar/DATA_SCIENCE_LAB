a530abcd0de11b98726727cc409eaa22c981e1f7
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-10-05 15:39:03 +0530

    Update factorial.py

--- Ds_labcycle1/factorial.py
+++ Ds_labcycle1/factorial.py
@@ -6,3 +6,9 @@ def fact(n):
         return(n*fact(n-1))
 n=int(input("Enter a number:"))
 print("Factorial : ",fact(n))
+
+//OUTPUT_______________________
+
+Demo of function:program to find factorial of a number
+Enter a number:5
+Factorial :  120
