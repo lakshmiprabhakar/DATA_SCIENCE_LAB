965f9aa14d2724c23ca02089bd3600333cffb549
lakshmiprabhakar <117815215+lakshmiprabhakar@users.noreply.github.com>
2023-10-05 14:12:39 +0530

    Update boolean.py

--- boolean.py
+++ boolean.py
@@ -9,3 +9,15 @@ print("Logical AND operation : ",t and f)
 print("Logical OR operation : ",t or f)
 print("Logical NOT operation : ",not f)
 print("Logical XOR operation : ",t!=f)
+
+//OUTPUT______________________
+Demo of basic data types : Boolean
+t= True
+f= False
+Data type of variable t :  <class 'bool'>
+Data type of variable f :  <class 'bool'>
+Logical AND operation :  False
+Logical OR operation :  True
+Logical NOT operation :  True
+Logical XOR operation :  True
+
